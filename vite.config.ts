b4bbb3b9f8a9fdadbc192a732bbbc6ff22cb94ce
bsides <rafaelbsides at gmail>
2025-08-28 04:55:29 -0300

    Inits the repository

---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'node:path'
+import dts from 'vite-plugin-dts'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), dts({ include: ['lib'] })],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats: ['es'],
+      name: 'streamerbot-react',
+    },
+    rollupOptions: {
+      external: [
+        'react',
+        'react/jsx-runtime',
+        'react-dom',
+        '@streamerbot/client',
+      ],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+        format: 'esm',
+        preserveModules: true,
+        interop: 'auto',
+      },
+    },
+  },
+})
